8b4cfb443bbd8f37b4a1c5de1c9667a2d314d14c
MORSTUAA <forza5horizon54262@gmail.com>
2025-01-10 14:57:37 +0200

    43
    33

---
+++ js/title.js
@@ -0,0 +1,35 @@
+﻿if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 300);
+
+}
+
+var x = 0;
+
+var titleText = [ "|",
+					"M|",
+					"M |",
+					"M o|",
+					"M o |",
+					"M o r|",
+					"M o r |",
+					"M o r s|",
+					"M o r s |",
+					"M o r s |",
+					"M o r s  |",	];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
